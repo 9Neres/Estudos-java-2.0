1e397a0c07e7f9c963c422f933cf770eb5f38a57
9Neres <ricardoborgescontact@gmail.com>
2025-06-13 00:03:41 -0300

    add package supplier.ex02

--- src/methodRefence/Main.java
+++ src/methodRefence/Main.java
@@ -1,4 +1,27 @@
 package methodRefence;
 
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class Main {
+    public static void main (String [] args){
+
+        Supplier<String> comprimento = () -> "Olá";
+
+        List<String> ListaSaudacao = Stream.generate(() -> "Olá")
+
+                .limit(4)
+                .toList();
+
+        ListaSaudacao.forEach(System.out::println);
+
+        Supplier<Integer> idade = () -> 19;
+
+        List<Integer> ListaIdade = Stream.generate(idade)
+                .limit(3)
+                .toList();
+
+        ListaIdade.stream().forEach(System.out::println);
+    }
 }
