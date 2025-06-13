78565f3bd3b71533cb452ee17769061eeb4af5a1
9Neres <ricardoborgescontact@gmail.com>
2025-06-12 21:08:11 -0300

    add package consumer.ex01

---
+++ src/lambdaExpression/supplier/ex02/Main.java
@@ -0,0 +1,27 @@
+package lambdaExpression.supplier.ex02;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main (String [] args){
+
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        Consumer<Integer> imprimirNumerosPar = numero -> {
+            if (numero % 2 == 0 ){
+                System.out.println(numero);
+            }
+        };
+
+        numeros.stream().forEach( n -> {
+                if(n % 2 == 0){
+                    System.out.println(n);
+                }
+            }
+        );
+
+
+    }
+}
