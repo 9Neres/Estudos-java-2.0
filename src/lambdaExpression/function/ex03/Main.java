7cf6953a083ce216bad976c7396f4f29d03bcad4
9Neres <ricardoborgescontact@gmail.com>
2025-06-13 11:54:16 -0300

    add code to package function

--- src/lambdaExpression/function/ex03/Main.java
+++ src/lambdaExpression/function/ex03/Main.java
@@ -3,19 +3,19 @@ package lambdaExpression.function.ex03;
 import java.util.List;
 import java.util.Arrays;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main (String [] args){
 
         List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9);
 
+        //Usar a função function com expressão lambda para dobrar todos os numeros
         Function<Integer, Integer> dobrar = numero -> numero * 2;
 
         List<Integer> numerosDobrados = numeros.stream()
-                .map(dobrar)
-                .collect(Collectors.toList());
+                .map(n -> n * 2) // map e uma função que dobra no stream e armazena em outra lista
+                .toList();
 
-        numerosDobrados.stream().forEach(System.out::println);
+        numerosDobrados.forEach(n -> System.out.println(n));
     }
 }
