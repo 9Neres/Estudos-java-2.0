4e37999931092f138ed4241f7acd50c0127edac1
9Neres <ricardoborgescontact@gmail.com>
2025-06-13 00:20:57 -0300

    realocked package

--- src/lambdaExpression/consumer/ex01/Main.java
+++ src/lambdaExpression/consumer/ex01/Main.java
@@ -1,9 +1,27 @@
 package lambdaExpression.consumer.ex01;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Main {
     public static void main (String [] args){
+
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        Consumer<Integer> imprimirNumerosPar = numero -> {
+            if (numero % 2 == 0 ){
+                System.out.println(numero);
+            }
+        };
+
+        numeros.stream().forEach( n -> {
+                if(n % 2 == 0){
+                    System.out.println(n);
+                }
+            }
+        );
 
 
     }
 }
-
--- src/lambdaExpression/supplier/ex02/Main.java
+++ src/lambdaExpression/supplier/ex02/Main.java
@@ -1,27 +1,27 @@
 package lambdaExpression.supplier.ex02;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main (String [] args){
 
-        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        Supplier<String> comprimento = () -> "Olá";
 
-        Consumer<Integer> imprimirNumerosPar = numero -> {
-            if (numero % 2 == 0 ){
-                System.out.println(numero);
-            }
-        };
+        List<String> ListaSaudacao = Stream.generate(() -> "Olá")
 
-        numeros.stream().forEach( n -> {
-                if(n % 2 == 0){
-                    System.out.println(n);
-                }
-            }
-        );
+                .limit(4)
+                .toList();
 
+        ListaSaudacao.forEach(System.out::println);
 
+        Supplier<Integer> idade = () -> 19;
+
+        List<Integer> ListaIdade = Stream.generate(idade)
+                .limit(3)
+                .toList();
+
+        ListaIdade.stream().forEach(System.out::println);
     }
 }
