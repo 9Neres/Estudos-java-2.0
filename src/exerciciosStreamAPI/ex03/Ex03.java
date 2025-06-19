cb0bff33869721b3c23e9790cfdc550995b7b94f
9Neres <ricardoborgescontact@gmail.com>
2025-06-19 20:26:14 -0300

    ex04

--- src/exerciciosStreamAPI/ex03/Ex03.java
+++ src/exerciciosStreamAPI/ex03/Ex03.java
@@ -1,4 +1,18 @@
 package exerciciosStreamAPI.ex03;
 
-public class Ex03 {
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex03{
+    public static void main(String[] args) {
+//        Desafio 3 - Verifique se todos os números da lista são positivos:
+//        Com a ajuda da Stream API, verifique se todos os números da lista são positivos e exiba o resultado no console.
+
+        List<Integer> numeros = Arrays.asList(-1,-2,-3,-4,1,2,3,4);
+
+        numeros.stream()
+                .filter(n -> n > 0)
+                .forEach(System.out::println);
+
+    }
 }
--- src/exerciciosStreamAPI/ex04/Ex04.java
+++ src/exerciciosStreamAPI/ex04/Ex04.java
@@ -1,4 +1,25 @@
 package exerciciosStreamAPI.ex04;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Ex04 {
+    public static void main(String[] args) {
+
+//        Desafio 4 - Remova todos os valores pares:
+//        Utilize a Stream API para remover os valores pares da lista e imprima a lista resultante no console.
+
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        numeros.stream()
+                .filter(n -> n % 2 != 0)
+                .forEach(System.out::println);
+
+        int numero = numeros.stream()
+                .filter(n -> n % 2 != 0)
+                .reduce(0, Integer::sum);
+
+        System.out.println(numero);
+
+    }
 }
